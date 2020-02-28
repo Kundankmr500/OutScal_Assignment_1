3f5d6081ca0fccecc2002a291b04ef45df1512f7
Kundan kumar <30592698+Kundankmr500@users.noreply.github.com>
2020-02-28 17:52:19 +0530

    Implemented Button in Master Scene with 3 different scenes

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void LoadNewScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
